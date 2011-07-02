037acabaa57d19227d0dc4e25c569ca407e469cb
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2011-07-02 15:33:07 +0100

    Start CLI client code.

---
+++ haulage.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# 
+# Haulage 
+# Dependency manager for your source code.
+#
+# Copyright (c) 2011 Sam Saint-Pettersen.
+# Released under the MIT/X11 License.
+#
+require 'optparse'
+require 'pp'
+#require 'monogodb'
+
+def main()
+	puts 'Welcome to Haulage!'
+end
+
+main()
