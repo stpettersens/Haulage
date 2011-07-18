b41bc9684138668b8b9c336bd9b7fcc797c849a2
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2011-07-18 13:39:39 +0000

    Google +1 button

--- haulage.js
+++ haulage.js
@@ -29,7 +29,8 @@ function onLoad() {
 	$('div#footer').append('<p>Enhanced with '
 	+ '<a class="flink" href="http://jquery.com">jQuery</a> '
 	+ 'and <a class="flink" href="http://github.com/stpettersens/ClipboardApplet">'
-	+ 'ClipboardApplet</a>.</p>');
+	+ 'ClipboardApplet</a>.</p><p><span style="padding-left: 30%;"><g:plusone>'
+    + '</g:plusone></span></p>');
 
 	// Make all "extern" and "flink" class links open in new window (target="_blank").
 	$('a.extern').attr('target', '_blank');
